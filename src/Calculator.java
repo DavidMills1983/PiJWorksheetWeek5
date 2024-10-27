9b85a7e90919ddabba9ff0eaf8be6efceea97703
David Mills <millsdg-gmail.com>
2024-10-27 15:08:46 +0000

    Separate class with calculator methods

---
+++ src/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+    double add (int x, int y){
+        return (double) x + y;
+    }
+
+    double subtract(int x, int y){
+        return (double) x - y;
+    }
+
+    double multiply(int x, int y){
+        return (double) x * y;
+    }
+
+    double divide(int x, int y){
+        return (double) x / (double) y;
+    }
+
+    double modulus(int x, int y){
+        return (double) x % (double) y;
+    }
+}
